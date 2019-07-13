e11830edd507e934a29ef90c7808ad350c1a0d41
rsimister <ryan.simister@gmail.com>
2019-07-13 22:12:17 +0100

    tutorial complete

--- bookmanager.py
+++ bookmanager.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -20,13 +20,39 @@ class Book(db.Model):
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    books = None
     if request.form:
-        book = Book(title=request.form.get("title"))
-        db.session.add(book)
-        db.session.commit()
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print("Failed to add book")
+            print(e)
     books = Book.query.all()
     return render_template("home.html", books=books)
 
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
